cd26293f4e3a38f96c104e9844da64882e926214
kyeongho <dagh1218@naver.com>
2024-07-10 00:36:23 +0900

    #13 메인 페이지 구성
    - 메인 페이지 내 코인 검색 및 검색 결과 컴포넌트 삽입

--- frontend/src/pages/CoinApp.jsx
+++ frontend/src/pages/CoinApp.jsx
@@ -1,12 +1,14 @@
 import React, { PureComponent } from "react";
 import AppLayout from "./AppLayout";
 import CoinOverview from "../components/CoinOverview";
+import CoinSearchList from "./../components/CoinSearchList";
 
 class CoinApp extends PureComponent {
   render() {
     return (
       <AppLayout>
         <CoinOverview />
+        <CoinSearchList />
       </AppLayout>
     );
   }
