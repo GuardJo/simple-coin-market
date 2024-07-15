fbc0788f93a75d88fc376ab29dab8bbbe3b0b1b9
kyeongho <dagh1218@naver.com>
2024-07-15 23:55:13 +0900

    #17 가상코인 관련 Entity 구성
    - 구성된 virtual_coin 테이블에 대한 hibernate 구현체 기반 jpa Entity 구성
    - 관련 설정 파일 구성 및 config bean 구성
    - 테스트 환경에선 ddl-auto create 로 설정
        - 일반 실행 환경에선 ddl-auto를 validate로 설정하여 정합성 체크

---
+++ coin-api-server/src/main/java/com/guardjo/simple/coin/market/apiserver/domain/BaseEntity.java
@@ -0,0 +1,17 @@
+package com.guardjo.simple.coin.market.apiserver.domain;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+	@CreatedDate
+	private LocalDateTime createTime;
+	@LastModifiedDate
+	private LocalDateTime modifiedTime;
+}
